081c7747a2b03122ac9ff7be665ccc18c1108c84
Paco <palmtreerooskee@gmail.com>
2017-04-03 17:35:52 -0400

    transferring work from Abby

--- src/TestTrack.js
+++ src/TestTrack.js
@@ -5,74 +5,63 @@ import './App.css';
 class TestTrack extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: '',
-      extendRate: 0,
-      firstStartTime: 0,
-      firstStandTime: 0,
-      firstStandEndTime: 0,
-      firstExtendEndTime: 0,
-      secondStartTime: 0,
-      secondStandTime: 0,
-      secondStandEndTime: 0,
-      secondExtendEndTime: 0,
-
-    };
+    this.state = {students: [], name: '', extendTime: 0, testName: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({name: event.target.value});
-  }
-
-  handleSubmit(event) {
-    // alert('A student was submitted: ' + this.state.name);
-
-    event.preventDefault();
-  }
-
   render() {
     return (
       <div>
+        <h3>Testing Room:</h3>
+        <Classroom students={this.state.students} />
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.name} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
+          <input type="text" onChange={this.handleChange} value={this.state.name} />
+          <select type="number" value={this.state.extendTime} onChange={this.handleChange}>
+            <option value={1.5}>1.5x</option>
+            <option value={2}>2x</option>
+            <option value={2.5}>2.5x</option>
+            <option value={3}>3x</option>
+          </select>
+          <button>{'Add student #' + (this.state.students.length + 1)}</button>
         </form>
-        <div className="testDashboard">
-          <p>students: </p>
-          <Classroom student={this.state.name}/>
-        </div>
       </div>
     );
+  }
+
+  handleChange(e) {
+    const value = e.target.type === "text" ? this.state.name : this.state.extendTime;
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var newStudent = {
+      name: this.state.name,
+      id: Date.now(),
+      extendTime: this.extendTime,
+      testName: this.testName
+    };
+    this.setState((prevState) => ({
+      students: prevState.students.concat(newStudent),
+      name: ''
+    }));
   }
 }
 
 class Classroom extends React.Component {
-  renderStudent() {
-    return <Student value={this.props.student} num={1}/>;
-  }
   render() {
     return(
-      <div>
-        <div className="student-row">
-          {this.renderStudent()}
-        </div>
-      </div>
+      <ul>
+        {this.props.students.map(student => (
+          <li key={student.id}>{student.name}: </li>
+        ))}
+      </ul>
     );
   }
 }
-
-function Student(props) {
-  return (
-    <li key={props.num} className="student">
-      {props.value}
-    </li>
-  );
-}
-
 export default TestTrack;
+// Contact GitHub API Training Shop Blog About
